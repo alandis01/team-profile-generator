d22dc5c8d7981d997b8446a38ee09d2df685321b
ashley <alandis01@gmail.com>
2023-01-17 20:07:30 -0500

    added const for manager, employee, and intern questions. Added questions for each profile / role

---
+++ index.js
@@ -0,0 +1,66 @@
+const inquirer = rquire('inquirer');
+const fs = require('fs');
+
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const prompt = inquirer.createPromptModule();
+const teamProfile = [];
+
+const managerQuestions = [
+    {
+        message: 'What is the manager\'s name?',
+        name: 'name',
+    },
+    {
+        message: 'What is the manager\'s id?',
+        name: 'id',
+    },
+    {
+        message: 'What is the manager\'s email?',
+        name: 'email',
+    },
+    {
+        message: 'What is the manager\'s office number?',
+        name: 'number',
+    },
+];
+
+const engineerQuestions = [
+    {
+        message: 'What is the engineer\'s name?',
+        name: 'name',
+    },
+    {
+        message: 'What is the engineer\'s id?',
+        name: 'id',
+    },
+    {
+        message: 'What is the engineer\'s email?',
+        name: 'email',
+    },
+    {
+        message: 'What is the engineer\'s GitHub name?',
+        name: 'gitHub',
+    },
+];
+
+const internQuestions = [
+    {
+        message:'What is the intern\'s name?',
+        name: 'name',
+    },
+    {
+        message: 'What is the intern\'s id?',
+        name: 'id',
+    },
+    {
+        message:'What is the intern\'s email?',
+        name: 'email',
+    },
+    {
+        message: 'Where does the intern go to school?',
+        name: 'school',
+    },
+];
