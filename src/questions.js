45abf2687b0fe21fe988e1d1383d4ecb2d144d7a
ashley <alandis01@gmail.com>
2023-01-19 18:57:50 -0500

    made questions.js and utils.js files. Copied questions from index.js and put them into quesitons.js

---
+++ src/questions.js
@@ -0,0 +1,58 @@
+const managerQuestions = [
+    {
+        message: 'What is the manager\'s name?',
+        name: 'name',
+    },
+    {
+        message: 'What is the manager\'s id?',
+        name: 'id',
+    },
+    {
+        message: 'What is the manager\'s email?',
+        name: 'email',
+    },
+    {
+        message: 'What is the manager\'s office number?',
+        name: 'phone',
+    },
+];
+
+const engineerQuestions = [
+    {
+        message: 'What is the engineer\'s name?',
+        name: 'name',
+    },
+    {
+        message: 'What is the engineer\'s id?',
+        name: 'id',
+    },
+    {
+        message: 'What is the engineer\'s email?',
+        name: 'email',
+    },
+    {
+        message: 'What is the engineer\'s GitHub name?',
+        name: 'gitHub',
+    },
+];
+
+const internQuestions = [
+    {
+        message: 'What is the intern\'s name?',
+        name: 'name',
+    },
+    {
+        message: 'What is the intern\'s id?',
+        name: 'id',
+    },
+    {
+        message: 'What is the intern\'s email?',
+        name: 'email',
+    },
+    {
+        message: 'Where does the intern go to school?',
+        name: 'school',
+    },
+];
+
+module.exports = { engineerQuestions, internQuestions, managerQuestions }
